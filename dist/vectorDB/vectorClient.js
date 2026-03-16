41c949d448e9ddb5ab9da082c040651198c6ede6
sandro <akecakabro@gmail.com>
2026-03-16 14:16:48 +0100

    remove dist from tracking

--- dist/vectorDB/vectorClient.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.searchEmbeddings = searchEmbeddings;
-// Esempio placeholder:
-async function searchEmbeddings(query) {
-    return [];
-}
-// In futuro collega Pinecone
